a0f1dee8ea981ce5fcbca6320e88ab612ed00f4a
Sakthivel Ganesan <Sms2sakthivel@outlook.com>
2024-12-28 20:14:43 +0530

    File Structre Changes done

--- products/model/productdb.go
+++ products/model/productdb.go
@@ -1,11 +1,14 @@
 package model
 
+import "gorm.io/gorm"
+
 type Product struct {
+	gorm.Model
 	ID          uint   `gorm:"primaryKey"`
-	Name        string `gorm:"size:100;not null"`
-	Brand       string `gorm:"size:100;not null"`
-	Category    string `gorm:"size:100;not null"`
-	SubCategory string `gorm:"size:100;not null"`
+	Name        string `gorm:"not null"`
+	Brand       string `gorm:"not null"`
+	Category    string `gorm:"not null"`
+	SubCategory string `gorm:"not null"`
 	Price       uint   `gorm:"not null"`
 }
 
